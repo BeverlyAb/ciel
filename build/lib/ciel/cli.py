cb9e2d8a2fe9c3a48cea38dc45187c75159dc94c
babadine <bev.abadines@gmail.com>
2021-04-23 16:05:32 -0700

    ciel commands appear!

--- build/lib/ciel/cli.py
+++ build/lib/ciel/cli.py
@@ -18,48 +18,57 @@ import ciel.config
 from ciel import CIEL_VERSION_STRING
 import sys
 
+
 def start_master(args):
     ciel.runtime.master.main(args)
+
 
 def start_worker(args):
     ciel.runtime.worker.main(args)
 
+
 def run_job(args):
     ciel.runtime.util.start_job.main(args)
+
 
 def run_jar(args):
     ciel.runtime.util.start_job.jar(args)
 
+
 def run_sw(args):
     ciel.runtime.util.run_script.main(args)
+
 
 def config(args):
     ciel.config.main(args)
 
+
 def show_help(args):
-    print >>sys.stderr, "usage: ciel COMMAND [ARGS]"
-    print >>sys.stderr
-    print >>sys.stderr, "The main Ciel commands are:"
+    print("usage: ciel COMMAND [ARGS]", file=sys.stderr)
+    print(file=sys.stderr)
+    print("The main Ciel commands are:", file=sys.stderr)
     for command, _, description in default_command_list:
         if description is not None:
-            print >>sys.stderr, '   %s %s' % (command.ljust(10), description)
+            print('   %s %s' % (command.ljust(10), description), file=sys.stderr)
+
 
 def version():
-    print >>sys.stderr, CIEL_VERSION_STRING
+    print(CIEL_VERSION_STRING, file=sys.stderr)
 
-default_command_list = [('master',    start_master, "Start running a CIEL master"),
-                        ('worker',    start_worker, "Start running a CIEL worker"),
-                        ('run',       run_job,      "Run a CIEL job"),
-                        ('java',      run_jar,      "Run a Java-based CIEL job"),
-                        ('sw',        run_sw,       "Run a Skywriting script"),
-                        ('config',    config,       "Configure CIEL"),
-                        ('help',      show_help,    "Display this message"),
-                        ('--version', version,      None)]
+
+default_command_list = [('master', start_master, "Start running a CIEL master"),
+                        ('worker', start_worker, "Start running a CIEL worker"),
+                        ('run', run_job, "Run a CIEL job"),
+                        ('java', run_jar, "Run a Java-based CIEL job"),
+                        ('sw', run_sw, "Run a Skywriting script"),
+                        ('config', config, "Configure CIEL"),
+                        ('help', show_help, "Display this message"),
+                        ('--version', version, None)]
 
 default_command_map = dict([(x, (y, z)) for x, y, z in default_command_list])
 
-def main():
 
+def main():
     my_args = sys.argv[:]
 
     if len(my_args) < 2:
@@ -72,13 +81,13 @@ def main():
             func, _ = default_command_map[command]
             exit_code = 0
         except KeyError:
-            print >>sys.stderr, 'Unrecognised command: %s' % command
+            print('Unrecognised command: %s' % command, file=sys.stderr)
             func = show_help
             exit_code = -1
 
     func(my_args)
     sys.exit(exit_code)
-    
+
 
 if __name__ == '__main__':
     main()
