9282d8d56e5fb447bb245021743865a79a8333e9
babadine <bev.abadines@gmail.com>
2021-04-23 16:03:56 -0700

    Queue to queue

--- build/lib/ciel/runtime/util/attach.py
+++ build/lib/ciel/runtime/util/attach.py
@@ -18,62 +18,68 @@ import simplejson
 from urllib.parse import urlparse
 import os
 
-PROTOCOLS = {'json' : True, 'protobuf' : True, 'pickle' : True, 'line' : True}
-FORMATS = {'json' : True, 'env' : True, 'protobuf' : True, 'pickle' : True}
+PROTOCOLS = {'json': True, 'protobuf': True, 'pickle': True, 'line': True}
+FORMATS = {'json': True, 'env': True, 'protobuf': True, 'pickle': True}
+
 
 def attach(worker_uri, pid, protocol='json'):
-    
     h = httplib2.Http()
     process_request = simplejson.dumps((pid, protocol))
     try:
         response, content = h.request(urlparse.urljoin(worker_uri, '/control/process/'), 'POST', process_request)
     except:
-        print >>sys.stderr, "Error: couldn't contact worker"
+        print("Error: couldn't contact worker", file=sys.stderr)
         return None
 
     if response['status'] != '200':
-        print >>sys.stderr, "Error: non-OK status from worker (%s)" % response['status']
+        print("Error: non-OK status from worker (%s)" % response['status'], file=sys.stderr)
         return None
 
     else:
         return simplejson.loads(content)
 
-def render(descriptor, format='json'):
 
+def render(descriptor, format='json'):
     if format == 'json':
-        print simplejson.dumps(descriptor)
+        print(simplejson.dumps(descriptor))
     elif format == 'env':
-        print 'export CIEL_PROCESS_ID=%s' % descriptor['id']
-        print 'export CIEL_PROCESS_PROTOCOL=%s' % descriptor['protocol']
-        print 'export CIEL_PIPE_TO_WORKER=%s' % descriptor['to_worker_fifo']
-        print 'export CIEL_PIPE_FROM_WORKER=%s' % descriptor['from_worker_fifo']
-        
+        print('export CIEL_PROCESS_ID=%s' % descriptor['id'])
+        print('export CIEL_PROCESS_PROTOCOL=%s' % descriptor['protocol'])
+        print('export CIEL_PIPE_TO_WORKER=%s' % descriptor['to_worker_fifo'])
+        print('export CIEL_PIPE_FROM_WORKER=%s' % descriptor['from_worker_fifo'])
+
     else:
-        print >>sys.stderr, 'Format not yet supported: %s' % format
+        print('Format not yet supported: %s' % format, file=sys.stderr)
         raise
 
 
 def main():
     parser = optparse.OptionParser(usage='sw-attach [options] -P PID')
-    parser.add_option("-w", "--worker", action="store", dest="worker", help="Worker URI", metavar="WORKER", default='http://localhost:8001/')
-    parser.add_option("-P", "--pid", action="store", dest="pid", help="Process ID", metavar="PID", type="int", default=os.getppid())
-    parser.add_option("-p", "--protocol", action="store", dest="protocol", help="IPC protocol to use. Valid protocols are json (default), protobuf or pickle", default='json')
-    parser.add_option("-F", "--format", action="store", dest="format", help="Format to write out umbilical details. Valid formats are json (default), env, protobuf or pickle", default='json')
+    parser.add_option("-w", "--worker", action="store", dest="worker", help="Worker URI", metavar="WORKER",
+                      default='http://localhost:8001/')
+    parser.add_option("-P", "--pid", action="store", dest="pid", help="Process ID", metavar="PID", type="int",
+                      default=os.getppid())
+    parser.add_option("-p", "--protocol", action="store", dest="protocol",
+                      help="IPC protocol to use. Valid protocols are json (default), protobuf or pickle",
+                      default='json')
+    parser.add_option("-F", "--format", action="store", dest="format",
+                      help="Format to write out umbilical details. Valid formats are json (default), env, protobuf or pickle",
+                      default='json')
     (options, _) = parser.parse_args()
 
     should_exit = False
-    if options.protocol not in PROTOCOLS.keys():
-        print >>sys.stderr, "Error: must specify a valid protocol"
+    if options.protocol not in list(PROTOCOLS.keys()):
+        print("Error: must specify a valid protocol", file=sys.stderr)
         should_exit = True
-        
-    if options.format not in FORMATS.keys():
-        print >>sys.stderr, "Error: must specify a valid format"
+
+    if options.format not in list(FORMATS.keys()):
+        print("Error: must specify a valid format", file=sys.stderr)
         should_exit = True
 
     if options.pid is None:
-        print >>sys.stderr, "Error: must specify a process ID"
+        print("Error: must specify a process ID", file=sys.stderr)
         should_exit = True
-        
+
     if should_exit:
         parser.print_help()
         sys.exit(-1)
@@ -85,6 +91,7 @@ def main():
         else:
             render(descriptor, options.format)
             sys.exit(0)
-        
+
+
 if __name__ == '__main__':
     main()
